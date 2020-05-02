07fcaf21135d499700b646898743c67f6488ead2
AcasiaWood <57312000+AcasiaWood@users.noreply.github.com>
2020-05-02 16:44:24 +0900

    Delete subtraction.py

--- Algorithm/Math/subtraction.py
+++
@@ -1,7 +0,0 @@
-a_location = [(1, 1), (2, 2), (3, 3)]
-b_location = [(1, 1), (2, 2), (3, 3)]
-for i in range(3):
-    a = a_location[0]
-    b = a_location[2]
-    a_location[0] = b
-    a_location[2] = a
