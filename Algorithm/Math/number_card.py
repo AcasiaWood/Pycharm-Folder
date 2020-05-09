e427d13acaab9bb36cd5e89280ddeeb18a792acc
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-09 17:02:32 +0900

    Create number_card.py

---
+++ Algorithm/Math/number_card.py
@@ -0,0 +1,12 @@
+m = int(input())
+card_list = list(input().split())
+n = int(input())
+number_list = list(input().split())
+string = ''
+for number in range(len(number_list)):
+    if number_list[number] in card_list:
+        number_list[number] = 1
+    else:
+        number_list[number] = 0
+    string += str(number_list[number]) + ' '
+print(string)
