039339e09e0e2f6a4084a885f6a3239b4ca34b9c
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-09 15:04:25 +0900

    Update dictionary.py

--- Basic/dictionary/dictionary.py
+++ Basic/dictionary/dictionary.py
@@ -1,7 +1,7 @@
 # create dictionary
 
-country = {'korea': '대한민국', 'united kingdom': '영국', 'india': '인도'}
-print(country)
+countries = {'korea': '대한민국', 'united kingdom': '영국', 'india': '인도'}
+print(countries)
 
 # add dictionary value
 
@@ -16,3 +16,19 @@ del multiple[1]
 print(multiple)
 
 # get key and value
+
+fruits = ['apple': '사과', 'orange': '오랜지', 'pineapple': '파인애플']
+print(fruits['apple'])
+print(fruits['사과'])
+
+# get all keys and values
+
+vegetables = ['carrot': '당근', 'potato': '감자', 'corn': '옥수수']
+print(vegetables.keys())
+print(vegetables.values())
+
+# clear dictionary value
+
+numbers = {1: 2, 3: 4, 5: 6}
+numbers.clear()
+print(numbers)
