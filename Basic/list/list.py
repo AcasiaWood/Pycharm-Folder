e0ef19149a3c64fbf818629b5ee8e989c05c4a3b
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-09 10:46:38 +0900

    Update list.py

--- Basic/list/list.py
+++ Basic/list/list.py
@@ -1,1 +1,22 @@
+# a way to use a list
+
+numbers = [1, 2, 3]
+
+# list indexing
+
+grades = [93, 48, 64]
+print(grades[0])
+
+# list slicing
+
+toys = ['car', 'robot', 'block']
+print(toys[0:2])
+
+# add list
+
+prime_numbers = [2, 3, 5]
+synthetic_numbers = [4, 6, 8]
+print(prime_numbers + synthetic_numbers)
+
+# multiply list
 
