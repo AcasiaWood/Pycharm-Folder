dcc1ca68f670777bc96740bcf47be09fa425de87
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-09 13:10:41 +0900

    Update and rename dictionary_01.py to dictionary.py

---
+++ Basic/dictionary/dictionary.py
@@ -0,0 +1,18 @@
+# create dictionary
+
+country = {'korea': '대한민국', 'united kingdom': '영국', 'india': '인도'}
+print(country)
+
+# add dictionary value
+
+numbers = {1: -1, 2: -2, 3: -3}
+a[4] = -4
+print(numbers)
+
+# delete dictionary value
+
+multiple = {1: 2, 2: 4, 3: 6}
+del multiple[1]
+print(multiple)
+
+# et key and value
--- Basic/dictionary/dictionary_01.py
+++
@@ -1,3 +0,0 @@
-a = {}
-a['Korea'] = '한국'
-print(a.keys(), a.values())
