c53803948332dd559b1efa5b58e57afbba6876aa
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-16 15:04:53 +0900

    Update quadratic_equation.py

--- Algorithm/Math/quadratic_equation.py
+++ Algorithm/Math/quadratic_equation.py
@@ -9,7 +9,7 @@ else:
         first_root = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
         second_root = (-b - math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
         if first_root == second_root:
-            print('multiple root = ' + first_root)
+            print('multiple root = ' + str(first_root))
         else:
             print('roots = ' + str(first_root) + ' or ' + str(second_root))
     else:
