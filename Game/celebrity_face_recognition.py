874b444fb3a4006fa01e496aa88f2531415412ab
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-05-26 17:27:11 +0900

    Update celebrity_face_recognition.py

--- Game/celebrity_face_recognition.py
+++ Game/celebrity_face_recognition.py
@@ -1,13 +1,11 @@
-import os
-import sys
 import requests
 
-client_id = "여기를 지우고 클라이언트 아이디를 넣으세요."
-client_secret = "여기를 지우고 클라이언트 시크릿을 넣으세요."
+client_id = "No ID was entered to protect personal information."
+client_secret = "No password was entered to protect personal information."
 
 url = "https://openapi.naver.com/v1/vision/celebrity"
 
-files = {'image': open('여기를 지우고 확장자를 포함한 파일 이름을 넣으세요.', 'rb')}
+files = {'image': open('sample.png', 'rb')}
 headers = {'X-Naver-Client-ID': client_id, 'X-Naver-Client-Secret': client_secret}
 response = requests.post(url, files=files, headers=headers)
 rescode = response.status_code
