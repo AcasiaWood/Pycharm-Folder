c7f990968a70c980e187efe811f768c7586f3919
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-22 18:31:07 +0900

    Update writer.py

--- Project/A.I/document/writer.py
+++ Project/A.I/document/writer.py
@@ -8,15 +8,15 @@ path = "/home/" + user + "/다운로드/"
 file = open("document/list.csv", 'w')
 file.write("filename,label\n")
 
-for d1 in os.listdir("/home/" + user + "/다운로드/"):
+for d1 in os.listdir(path):
     r1 = path + d1
     if d1 == "음식물쓰레기":
         num = 0
     else:
         num = 1
-    for d2 in os.listdir("/home/" + user + "/다운로드/" + d1):
+    for d2 in os.listdir(r1):
         r2 = r1 + '/' + d2
-        for d3 in os.listdir("/home/" + user + "/다운로드/" + d1 + '/' + d2):
+        for d3 in os.listdir(r2):
             r3 = r2 + '/' + d3
             file.write(r3 + "," + str(num) + "\n")
 
