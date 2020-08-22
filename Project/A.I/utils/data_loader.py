05cbd4dc9e0b0e404ae781fed3507d76ba89fcb2
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-22 18:28:35 +0900

    Update data_loader.py

--- Project/A.I/utils/data_loader.py
+++ Project/A.I/utils/data_loader.py
@@ -4,32 +4,26 @@ import pandas as pd
 import numpy as np
 from PIL import Image
 import torch
-from torch.utils.data.sampler import SubsetRandomSampler
 
 
 class NkDataSet(Dataset):
 
     def __init__(self, file_path):
         self.trans = transforms.Compose([transforms.RandomHorizontalFlip(),
-                                         transforms.RandomCrop(128, 4),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                              std=[0.229, 0.224, 0.225])])
+                                         transforms.ToTensor()
+                                        ])
         self.to_tensor = transforms.ToTensor()
         self.data_info = pd.read_csv(file_path, header=None)
         self.image_arr = np.asarray(self.data_info.iloc[:, 0][1:])
-        self.label_arr = np.asarray(self.data_info.iloc[:, 1][1:])
-        self.label_arr = self.label_arr.astype(np.long) - 1
+        self.label_arr = np.asarray(self.data_info.iloc[:, 1][1:], dtype=np.float32)
         self.label_arr = torch.from_numpy(self.label_arr)
         self.data_len = len(self.data_info.index)
-        self.img_path = "/tmp/pycharm_project_917/img/faces_images/"
 
     def __getitem__(self, index):
-        img_name = self.img_path + self.image_arr[index - 1]
+        img_name = self.image_arr[index - 1]
         img_as_img = Image.open(img_name)
         img_as_tensor = self.trans(img_as_img)
         img_label = self.label_arr[index - 1]
-
         return img_as_tensor, img_label, img_name
 
     def __len__(self):
@@ -37,34 +31,27 @@ class NkDataSet(Dataset):
 
 
 def get_data_loader(args):
-    validation_split = 2
-    random_seed = 42
 
-    csv_path = "/tmp/pycharm_project_917/files/train_vision.csv"
+    csv_path = "path"
     custom_dataset = NkDataSet(csv_path)
 
-    test_csv_path = "/tmp/pycharm_project_917/files/test_vision.csv"
+    test_csv_path = "path"
     test_custom_dataset = NkDataSet(test_csv_path)
 
-    dataset_size = len(custom_dataset)
-    indices = list(range(dataset_size))
-    split = int(np.floor(validation_split * dataset_size))
-    np.random.seed(random_seed)
-    np.random.shuffle(indices)
-
-    train_indices, val_indices = indices[split:], indices[:split]
-
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-
-    train_loader = torch.utils.data.DataLoader(custom_dataset, batch_size=64,
-                                               sampler=train_sampler)
-
-    val_loader = torch.utils.data.DataLoader(custom_dataset, batch_size=64,
-                                             sampler=valid_sampler)
+    train_loader = torch.utils.data.DataLoader(dataset=custom_dataset, batch_size=1, shuffle=False)
 
     test_loader = torch.utils.data.DataLoader(dataset=test_custom_dataset, batch_size=1, shuffle=False)
 
     print("data set length = %d" % len(test_loader))
 
-    return train_loader, val_loader, test_loader
+    return train_loader, test_loader
+
+
+csv_path = "path"
+custom_dataset = NkDataSet(csv_path)
+my_dataset_loader = torch.utils.data.DataLoader(dataset=custom_dataset,
+                                                batch_size=1,
+                                                shuffle=False)
+
+for i, (images, labels, img_name) in enumerate(my_dataset_loader):
+    print(images.shape, labels, img_name)
