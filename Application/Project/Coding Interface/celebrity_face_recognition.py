d12864512cc9aa890f00a3c4d4120dcc2fbc5e95
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-23 20:52:09 +0900

    Update celebrity_face_recognition.py

--- Application/Project/Coding Interface/celebrity_face_recognition.py
+++ Application/Project/Coding Interface/celebrity_face_recognition.py
@@ -5,7 +5,7 @@ client_secret = "privacy"
 
 url = "https://openapi.naver.com/v1/vision/celebrity"
 
-files = {'image': open('sample.png', 'rb')}
+files = {'image': open('sample.jpg', 'rb')}
 headers = {'X-Naver-Client-ID': client_id, 'X-Naver-Client-Secret': client_secret}
 response = requests.post(url, files=files, headers=headers)
 rescode = response.status_code
