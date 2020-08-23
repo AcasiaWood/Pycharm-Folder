573c911dad37dbf47de7958e1933601732c4aa77
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-08-23 20:01:31 +0900

    Update translator.py

--- Application/Project/Coding Interface/translator.py
+++ Application/Project/Coding Interface/translator.py
@@ -6,8 +6,6 @@ client_secret = "No password was entered to protect personal information."
 
 language = ["ko", "ja", "zh-cn", "zh-tw", "hi", "en", "es", "fr", "de", "pt", "vi", "id", "fa", "ar", "mm", "th", "ru", "it"]
 
-# import notepad to translate (txt format)
-
 with open('source.txt', 'r', encoding='utf8') as f:
     srcText = f.read()
 
@@ -17,16 +15,12 @@ for i in range(0, len(language)):
     if language[i] == enter:
         print("Language Check Completed")
 
-# stored in the encText variable, the notepad language is Korean, and the language to be translated is English.
-
 encText = urllib.parse.quote(srcText)
 data = "source=ko&target="+enter+"&text=" + encText
 url = "https://openapi.naver.com/v1/papago/n2mt"
 request = urllib.request.Request(url)
 request.add_header("X-Naver-Client-Id", client_id)
 request.add_header("X-Naver-Client-Secret", client_secret)
-
-# data encryption, check whether the import is correct or not
 
 response = urllib.request.urlopen(request, data=data.encode("utf-8"))
 rescode = response.getcode()
