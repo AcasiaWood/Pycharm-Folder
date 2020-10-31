b114ea5f2f4da41377d8cc7712c5e397cb88a4c3
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2020-10-31 18:06:09 +0900

    Update binomial_coefficient.py

--- Algorithm/Mathematics/binomial_coefficient.py
+++ Algorithm/Mathematics/binomial_coefficient.py
@@ -1,17 +1,20 @@
 memory = [[-1]*100]*100
 
+
 def binomial_coefficient(n, k):
     if memory[n][k] != -1:
         return memory[n][k]
     if n == k or n == 0 or k == 0:
         memory[n][k] = 1
         return memory[n][k]
-    memory[n][k] = binomial_coefficient(n-1, k-1) + binomial_coefficient(n-1, k)
+    memory[n][k] = binomial_coefficient(n - 1, k - 1) + binomial_coefficient(n - 1, k)
     return memory[n][k]
+
 
 n = int(input())
 triangle = []
 
-for i in range(n+1):
+for i in range(n + 1):
     triangle.append(binomial_coefficient(n, i))
+    
 print(triangle)
