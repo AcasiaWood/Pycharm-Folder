916de0c8112031cc6776c30c0a6474832e0705b1
eunkyunghyun <57312000+eunkyunghyun@users.noreply.github.com>
2021-01-09 02:00:27 +0900

    Update gui.py

--- Project/Messenger/gui.py
+++ Project/Messenger/gui.py
@@ -10,7 +10,7 @@ def client_exit():
 class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.accept = True
+        self.accept = False
         self.email = ""
         self.password = ""
         self.account = {"Admin": "1234"}
@@ -22,6 +22,7 @@ class Window(Frame):
         self.times = [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]
         self.canvas = None
         self.label = None
+        self.tab = None
         self.master = master
         self.init_window()
 
@@ -36,100 +37,129 @@ class Window(Frame):
         menu.add_cascade(label="File", menu=file)
 
         file = Menu(menu)
-        file.add_command(label="Login", command=self.login)
-        menu.add_cascade(label="Account", menu=file)
+        if not self.accept:
+            file.add_command(label="Login", command=self.login)
+            menu.add_cascade(label="Account", menu=file)
+        else:
+            file.add_command(label="Logout", command=self.logout)
+            menu.add_cascade(label="Account", menu=file)
+
+            file = Menu(menu)
+            file.add_command(label="Like", command=self.like)
+            file.add_command(label="Dislike", command=self.dislike)
+            file.add_command(label="Write", command=self.write)
+            file.add_command(label="View", command=self.view)
+            menu.add_cascade(label="Interest", menu=file)
 
         self.accept = False
 
     def login(self):
         while not self.accept:
-            self.canvas = tkinter.Canvas(root, width=400, height=300, relief='raised')
+            self.tab = Tk()
+            self.tab.geometry("400x300")
+            self.tab.title("Login")
+
+            self.canvas = tkinter.Canvas(self.tab, width=400, height=300, relief='raised')
             self.canvas.pack()
 
-            title = tkinter.Label(root, text='Login')
+            title = tkinter.Label(self.tab, text='Login')
             title.config(font=('helvetica', 14))
             self.canvas.create_window(200, 25, window=title)
 
-            email_label = tkinter.Label(root, text='Email:')
+            email_label = tkinter.Label(self.tab, text='Email:')
             email_label.config(font=('helvetica', 10))
             self.canvas.create_window(125, 80, window=email_label)
-            email = tkinter.Entry(root)
+            email = tkinter.Entry(self.tab)
             self.canvas.create_window(225, 80, window=email)
 
-            password_label = tkinter.Label(root, text='Password:')
+            password_label = tkinter.Label(self.tab, text='Password:')
             password_label.config(font=('helvetica', 10))
             self.canvas.create_window(125, 120, window=password_label)
-            password = tkinter.Entry(root)
+            password = tkinter.Entry(self.tab)
             self.canvas.create_window(235, 120, window=password)
 
             self.pack(fill=BOTH, expand=1)
-            button = Button(root, text="Login", command=lambda: self.login_permission(email, password), bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+            button = Button(self.tab, text="Login", command=lambda: self.login_permission(email, password),
+                            bg='brown', fg='white', font=('helvetica', 9, 'bold'))
             button.place(x=170, y=170)
 
-            root.update()
-            root.mainloop()
+            self.tab.update()
+            self.tab.mainloop()
 
     def login_permission(self, email, password):
         self.email = email.get()
         self.password = password.get()
         try:
             if self.account[self.email] == self.password:
-                self.label = tkinter.Label(root, text='You successfully logged in!', font=('helvetica', 10))
+                self.label = tkinter.Label(self.tab, text='You successfully logged in!', font=('helvetica', 10))
                 self.canvas.create_window(200, 250, window=self.label)
                 self.accept = True
-                pass
+                self.tab.destroy()
+                self.tab.quit()
+                self.init_window()
             else:
-                self.label = tkinter.Label(root, text='Error: You entered the wrong password.', font=('helvetica', 10))
+                self.label = tkinter.Label(self.tab, text='Error: You entered the wrong password.', font=('helvetica', 10))
                 self.canvas.create_window(200, 250, window=self.label)
-                self.label = tkinter.Label(root, text="", font=('helvetica', 10))
-                pass
+                self.label = tkinter.Label(self.tab, text="", font=('helvetica', 10))
         except KeyError:
-            self.label = tkinter.Label(root, text="Error: You entered the wrong email.", font=('helvetica', 10))
+            self.label = tkinter.Label(self.tab, text="Error: You entered the wrong email.", font=('helvetica', 10))
             self.canvas.create_window(200, 250, window=self.label)
-            self.label = tkinter.Label(root, text="", font=('helvetica', 10))
-            pass
+            self.label = tkinter.Label(self.tab, text="", font=('helvetica', 10))
 
-    def register(self):
-        while not self.accept:
-            root = tkinter.Tk()
-            root.title("Register")
+    def logout(self):
+        self.accept = False
+        self.init_window()
 
-            self.canvas = tkinter.Canvas(root, width=400, height=300, relief='raised')
-            self.canvas.pack()
+    def like(self):
+        try:
+            index = int(input("Which post: "))
+            self.likes[index - 1] += 1
+            if self.email not in self.likes_account[index - 1]:
+                self.likes_account[index - 1].append(self.email)
+            self.refresh(index)
+        except IndexError:
+            print("Error: The post does not exist.")
+        print()
 
-            title = tkinter.Label(root, text='Register')
-            title.config(font=('helvetica', 14))
-            self.canvas.create_window(200, 25, window=title)
+    def dislike(self):
+        try:
+            index = int(input("Which post: "))
+            self.dislikes[index - 1] += 1
+            if self.email not in self.dislikes_account[index - 1]:
+                self.dislikes_account[index - 1].append(self.email)
+            self.refresh(index)
+        except IndexError:
+            print("Error: The post does not exist.")
+        print()
 
-            email_label = tkinter.Label(root, text='Email:')
-            email_label.config(font=('helvetica', 10))
-            self.canvas.create_window(125, 80, window=email_label)
-            email = tkinter.Entry(root)
-            self.canvas.create_window(225, 80, window=email)
+    def write(self):
+        post = str(input("Please enter your post: "))
+        self.texts.insert(0, post)
+        self.likes.insert(0, 0)
+        self.dislikes.insert(0, 0)
+        self.times.insert(0, datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        self.likes_account.insert(0, [])
+        self.dislikes_account.insert(0, [])
+        print()
 
-            password_label = tkinter.Label(root, text='Password:')
-            password_label.config(font=('helvetica', 10))
-            self.canvas.create_window(125, 120, window=password_label)
-            password = tkinter.Entry(root)
-            self.canvas.create_window(235, 120, window=password)
-
-            self.email = email.get()
-            self.password = password.get()
-
-            button = tkinter.Button(text='Register', command=self.register_permission, bg='brown', fg='white',
-                                    font=('helvetica', 9, 'bold'))
-            self.canvas.create_window(200, 180, window=button)
-
-    def register_permission(self):
-        self.email, self.password = map(str, input("Please enter your email and password: ").split())
-        if self.email not in self.account.keys():
-            self.account[self.email] = self.password
-            label = tkinter.Label(root, text='You successfully created an account!', font=('helvetica', 10))
-            self.canvas.create_window(200, 210, window=label)
-            self.accept = True
-        else:
-            label = tkinter.Label(root, text='Error: Duplicate email. Please try again.', font=('helvetica', 10))
-            self.canvas.create_window(200, 210, window=label)
+    def view(self):
+        f = open("post", 'w')
+        f = open("post", 'a')
+        f.write("========================================\n")
+        print("========================================")
+        for i, text in enumerate(self.texts):
+            print(str(i + 1) + ": " + text)
+            print("Like: {} {}, Dislike: {} {}".format(self.likes[i], self.likes_account[i],
+                                                       self.dislikes[i], self.dislikes_account[i]))
+            print("Date: {}".format(self.times[i]))
+            print("========================================")
+            f.write(str(i + 1) + ": " + text + "\n")
+            f.write("Like: {} {}, Dislike: {} {}\n".format(self.likes[i], self.likes_account[i],
+                                                         self.dislikes[i], self.dislikes_account[i]))
+            f.write("Date: {}\n".format(self.times[i]))
+            f.write("========================================\n")
+        f.close()
+        print()
 
     def refresh(self, index):
         modify = self.texts[index - 1]
